6eb931dc3806547d6a080a80b52d397e9466219c
pietroline <pietrobarberi@hotmail.it>
2022-02-15 15:03:47 +0100

    Visualizzazione dinamica dei messaggi

--- js/main.js
+++ js/main.js
@@ -89,10 +89,39 @@ const vue = new Vue(
                                                 }
                                             ],
                             },
-                        ]
-
+                        ],
+            showContact: {
+                name: 'Michela',
+                avatar: '_1',
+                visible: true,
+                messages:   [
+                                {
+                                    date: '10/01/2020 15:30:55',
+                                    text: 'Hai portato a spasso il cane?',
+                                    status: 'sent'
+                                },
+                                {
+                                    date: '10/01/2020 15:50:00',
+                                    text: 'Ricordati di dargli da mangiare',
+                                    status: 'sent'
+                                },
+                                {
+                                    date: '10/01/2020 16:15:22',
+                                    text: 'Tutto fatto!',
+                                    status: 'received'
+                                }
+                            ],
+            },
         },
         methods:{
+
+            drawsContact(indice){
+                this.showContact.name = this.contacts[indice].name; 
+                this.showContact.avatar = this.contacts[indice].avatar;  
+                this.showContact.visible = this.contacts[indice].visible;
+                this.showContact.messages = this.contacts[indice].messages;    
+            }
+
         }
     
     }
